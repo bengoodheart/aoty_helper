6882fdc9ecb8250e37e109669e893dfd6f9e7e10
bengoodheart <bgoodheart@fordham.edu>
2021-03-05 16:47:27 -0500

    readded year

--- aoty_entry.py
+++ aoty_entry.py
@@ -19,6 +19,7 @@ def inputter():
             album = input("Please input the album name: ")
             genre = input("Please input the genre: ")  
             label = input("Please input the label: ")
+            year = input("Please input the year: ")
             rating = input("Please rate from 1 to 10: ")
             
             ##makes sure the rating int is valid
@@ -38,11 +39,11 @@ def inputter():
             comments = input("Please input any comments you may have: ")
             
             #Pre-write summary
-            print("Entry recorded:", album, "by", artist,"| Genre:", genre,"| Label:", label, "| You rated it a ",rating,"/10")
+            print("Entry recorded:", album, "by", artist,"| Genre:", genre,"| Label:", label, "| Released:", year, "| You rated it a ",rating,"/10")
             print("You commented: ", comments)
             correct = input("Is this correct? y/n: ")
             if (correct.lower() != 'n'):
-                writer.writerow([artist, album, genre, label, rating, comments])
+                writer.writerow([artist, album, genre, label, rating, year, comments])
             else:
                 writer.writerow([])
                 continue
@@ -60,7 +61,7 @@ if fileExists('aoty.csv'):
 else:
     with open('aoty.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        writer.writerow(['Arist','Album Name','Genre', 'Label', 'Rating', 'Comments'])
+        writer.writerow(['Arist','Album Name','Genre', 'Label', 'Rating', 'Year', 'Comments'])
         inputter()
             
 print("\nThe session has concluded. Goodbye.\n")
